9a2ac87b6d3ad0247fd2c5d8c2175c84998e4f44
Olivia Lammam <olivia.l7475@gmail.com>
2022-06-30 18:18:23 -0700

    updated models

---
+++ models/reaction.js
@@ -0,0 +1,24 @@
+const { Schema, model, Types } = require('mongoose');
+
+const reactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxLength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Comment = model('Comment', CommentSchema);
---
+++ models/thought.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require('mongoose');
+
+const thoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        minLength: 1,
+        maxLength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    username: {
+        type: String,
+        required: true
+    },
+ 
+    reactions: [reactionSchema],
+},
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false,
+    }
+);
+
+reactionSchema.virtual('reactionCount').get(() => {
+    return this.reactions.length
+})
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought
